00c02092b5e1a6fce176680eb030210ea5849fbb
Lactem <lactem@outlook.com>
2014-08-16 23:03:03 -0700

    Bug fix

--- src/main/java/net/galaxygaming/dispenser/kit/KitBase.java
+++ src/main/java/net/galaxygaming/dispenser/kit/KitBase.java
@@ -33,7 +33,7 @@ public class KitBase implements Kit {
 	public void load() {
 		ConfigurationSection kitSection = game.getConfig().getConfigurationSection("kits." + configName);
 		String[] itemStrings = kitSection.getString("items").split(",");
-		for (int i = 0; i < items.length; i++) {
+		for (int i = 0; i < itemStrings.length; i++) {
 			items[i] = ItemUtil.getInstance().getItem(itemStrings[i]);
 		}
 	}
